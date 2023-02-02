d802392db1d0d8dc9c6b7e39bd31a77108abdbc4
Paras <paras.nagpal743@gmail.com>
2023-02-02 20:40:11 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import './App.css';
+import { Provider } from 'react-redux';
+import React from 'react';
+ import CakeContainer from './components/CakeContainer';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+      <CakeContainer/>
+      </div>
+    </Provider>
+   
   );
 }
 
