089895de56be9e6bf2cac3026b116d10e34cee54
Paras <paras.nagpal743@gmail.com>
2023-02-03 03:45:16 +0000

    completed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { legacy_createStore as createStore , applyMiddleware} from 'redux';
 import logger from 'redux-logger'
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from './rootreducer';
 
-const store = createStore(rootReducer,applyMiddleware(logger))
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
 export default store 
